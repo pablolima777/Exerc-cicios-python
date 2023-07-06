4491281ce0e682b058ea444d4353e00284ab8a09
pablolima777 <126361010+pablolima777@users.noreply.github.com>
2023-07-05 22:18:50 -0300

    Criei o ex001.py

---
+++ Exercícios/ex001.py
@@ -0,0 +1,6 @@
+#Descubra se o número digitado é par ou impar
+n1 = int(input('\033[1m\033[46m\033[46m Entre com o número que deseja verificar: \033[0m'))
+if n1 % 2 == 0:
+    print('O número é par')
+else:
+    print('O número é impar')
